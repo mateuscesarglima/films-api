8ce656a4bf03dc6a45f96531811b293188277f00
Mateus Cesar <mateuscesarglima@gmail.com>
2022-01-25 18:26:27 -0300

    Remote Repository

---
+++ src/test/java/com/films/api/films_api/FilmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.films.api.films_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
