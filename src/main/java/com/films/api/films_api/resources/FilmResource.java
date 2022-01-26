fb87c43a66ebfa3ae572e7bec0db89458f0c18eb
Mateus Cesar <mateuscesarglima@gmail.com>
2022-01-26 12:30:13 -0300

    Film Resource

--- src/main/java/com/films/api/films_api/resources/FilmResource.java
+++ src/main/java/com/films/api/films_api/resources/FilmResource.java
@@ -1,11 +1,46 @@
 package com.films.api.films_api.resources;
 
+import java.util.List;
+
+import com.films.api.films_api.entities.Film;
+import com.films.api.films_api.services.FilmServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping(value = "/films")
 public class FilmResource {
 
+    @Autowired
+    private FilmServices services;
 
-    
+    @GetMapping
+    public ResponseEntity<List<Film>> findAll(){
+
+        ResponseEntity<List<Film>> response = new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        List<Film> obj = services.findAll();
+
+        if(!obj.isEmpty()){
+            response = ResponseEntity.ok().body(obj);
+        }
+        return response;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Film> findById(@PathVariable Long id){
+
+        Film obj = services.findByid(id);
+
+        return ResponseEntity.ok().body(obj);
+
+
+    }
+
 }
--- src/main/java/com/films/api/films_api/services/FilmServices.java
+++ src/main/java/com/films/api/films_api/services/FilmServices.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FilmSerivce {
+public class FilmServices {
 
     @Autowired
     private FilmRepository repository;
