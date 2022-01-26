7a49fb4364653758333080a8d0a9be167d8a2777
Mateus Cesar <mateuscesarglima@gmail.com>
2022-01-26 10:18:10 -0300

    Film service

---
+++ src/main/java/com/films/api/films_api/services/FilmSerivce.java
@@ -0,0 +1,31 @@
+package com.films.api.films_api.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.films.api.films_api.entities.Film;
+import com.films.api.films_api.repositories.FilmRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FilmSerivce {
+
+    @Autowired
+    private FilmRepository repository;
+
+    public List<Film> findAll() {
+        return repository.findAll();
+    }
+
+    public Film findByid(Long id){
+
+        Optional<Film> obj = repository.findById(id);
+
+        return obj.get();
+
+
+    }
+
+}
