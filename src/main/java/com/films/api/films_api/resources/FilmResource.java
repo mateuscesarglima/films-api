8f109e6cd5cb8d5d76fe5fac1b78d10a7868a791
Mateus Cesar <mateuscesarglima@gmail.com>
2022-01-27 13:15:45 -0300

    Film delete

--- src/main/java/com/films/api/films_api/resources/FilmResource.java
+++ src/main/java/com/films/api/films_api/resources/FilmResource.java
@@ -8,6 +8,7 @@ import com.films.api.films_api.services.FilmServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +41,15 @@ public class FilmResource {
 
         return ResponseEntity.ok().body(obj);
 
+
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        
+        services.delete(id);
+
+        return ResponseEntity.noContent().build();
 
     }
 
--- src/main/java/com/films/api/films_api/services/FilmServices.java
+++ src/main/java/com/films/api/films_api/services/FilmServices.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import com.films.api.films_api.entities.Film;
+import com.films.api.films_api.exceptions.DatabaseException;
 import com.films.api.films_api.exceptions.ResourceNotFoundException;
 import com.films.api.films_api.repositories.FilmRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -29,7 +32,15 @@ public class FilmServices {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+    try{
+            repository.deleteById(id);
+        }catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
+
+    
 
 }
