252b3cdef6308cb2f91a20365705a5d56fcd2739
Mateus Cesar <mateuscesarglima@gmail.com>
2022-01-27 11:52:03 -0300

    User update

--- src/main/java/com/films/api/films_api/resources/UserResource.java
+++ src/main/java/com/films/api/films_api/resources/UserResource.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,6 +42,14 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         
         User obj = services.findById(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@RequestBody User obj, @PathVariable Long id){
+        
+        obj = services.update(obj, id);
 
         return ResponseEntity.ok().body(obj);
     }
